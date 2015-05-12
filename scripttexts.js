f3af57599f92770a37911c3345dd8a035b29b615
chaimw88 <chaim.wijnberg@student.uva.nl>
2015-05-12 14:26:52 +0200

    Create scripttexts.js

---
+++ scripttexts.js
@@ -0,0 +1,6 @@
+var div = document.getElementById("textfutureChaim");
+    div.textContent = "Below is my dreamsession";
+    var text = div.textContent;
+    
+var para = document.createElement("p");
+var element = document.getElementById("textBody");
